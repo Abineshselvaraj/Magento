e4b7713d5955d717d0831426c049180fa926c164
Abinesh Selvaraj <abinesh@gmail.com>
2026-04-18 16:44:28 +0530

    second

--- pub/index.php
+++ pub/index.php
@@ -176,7 +176,7 @@ body {
 </div>
 
 <div class="hero">
-    <h1>Welcome to Magento store</h1>
+    <h1>Welcome to Magento store ver 02</h1>
     <p>Discover latest products with great offers</p>
     <button>Shop Now</button>
 </div>
