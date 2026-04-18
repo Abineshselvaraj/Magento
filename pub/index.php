98d96364f7427bfe69b74c4e8a31216ebe9720a5
Abinesh Selvaraj <abinesh@gmail.com>
2026-04-18 16:37:48 +0530

    1st commit

--- pub/index.php
+++ pub/index.php
@@ -176,7 +176,7 @@ body {
 </div>
 
 <div class="hero">
-    <h1>Welcome to MyStore</h1>
+    <h1>Welcome to Magento store</h1>
     <p>Discover latest products with great offers</p>
     <button>Shop Now</button>
 </div>
